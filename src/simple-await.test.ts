5eefd3b2c1dec0f8da9624a6b7f0835357c4e3d7
Alex Andreev <alex.andreev.email@gmail.com>
2023-02-10 16:43:40 +0300

    Add some additional tests
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

--- src/simple-await.test.ts
+++ src/simple-await.test.ts
@@ -6,5 +6,26 @@ describe("simple await", () => {
     const result = await wait(promise);
 
     expect(result.data).toBe(1);
-  })
+  });
+
+  it("should not return error when resolved", async () => {
+    const promise = Promise.resolve(1);
+    const result = await wait(promise);
+
+    expect(result.error).toBeUndefined();
+  });
+
+  it("should return error when rejected", async () => {
+    const promise = Promise.reject(new Error("error"));
+    const result = await wait(promise);
+
+    expect(result.error).toBeInstanceOf(Error);
+  });
+
+  it("should not return data when rejected", async () => {
+    const promise = Promise.reject(new Error("error"));
+    const result = await wait(promise);
+
+    expect(result.data).toBeUndefined();
+  });
 });
